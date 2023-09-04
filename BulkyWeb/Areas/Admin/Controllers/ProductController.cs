678a05d1ea88f1f599d1d0ed0efda26ea390db37
Hypengineer <okanizi16@gmail.com>
2023-09-04 04:15:38 +0300

    DataTables API

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -135,5 +135,16 @@ namespace BulkyWeb.Areas.Admin.Controllers
             return RedirectToAction("Index");
 
         }
+
+
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new {data=objProductList});
+        }
+        #endregion
     }
 }
