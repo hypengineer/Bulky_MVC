4c0fa6d1148827b88c0fc99c2d68e1c0bec78223
Hypengineer <okanizi16@gmail.com>
2023-09-18 21:11:51 +0300

    DBInitializer in Action

--- Bulky.DataAccess/DbInitializer/DbInitializer.cs
+++ Bulky.DataAccess/DbInitializer/DbInitializer.cs
@@ -23,59 +23,58 @@ namespace Bulky.DataAccess.DbInitializer
             RoleManager<IdentityRole> roleManager,
             ApplicationDbContext db)
         {
-            _userManager = userManager;
             _roleManager = roleManager;
+            _userManager = userManager;
             _db = db;
-            
         }
+
 
         public void Initialize()
         {
 
 
-            // migrations if they are not applied
+            //migrations if they are not applied
             try
             {
                 if (_db.Database.GetPendingMigrations().Count() > 0)
                 {
-                    _db.Database.Migrate(); 
+                    _db.Database.Migrate();
                 }
             }
-            catch(Exception ex)
-            {
-                    
-            }
+            catch (Exception ex) { }
 
 
 
-            // create roles if they are not created
+            //create roles if they are not created
             if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
             {
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+            
 
-                // if roles are not created, create admin user as well
 
-                _userManager.CreateAsync(new ApplicationUser
+            //if roles are not created, then we will create admin user as well
+            _userManager.CreateAsync(new ApplicationUser
                 {
                     UserName = "admin@iziokan.com",
                     Email = "admin@iziokan.com",
                     Name = "Okan İZİ",
                     PhoneNumber = "05433148915",
-                    StreetAddress = "Admin Test Address",
+                    StreetAddress = "Barbaros1",
                     State = "Bahcelievler",
                     PostalCode = "34100",
                     City = "Istanbul"
                 }, "Admin123*").GetAwaiter().GetResult();
 
-                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@iziokan.com");
+
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@iziokn.com");
                 _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+
             }
 
             return;
-
         }
     }
 }
