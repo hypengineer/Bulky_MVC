a53ee49ae5ba51da697a8f527e0c44c65be8b938
Hypengineer <okanizi16@gmail.com>
2023-09-21 14:38:46 +0300

    Lock Unlock in Action

--- BulkyWeb/Areas/Admin/Controllers/UserController.cs
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -79,7 +79,7 @@ namespace BulkyWeb.Areas.Admin.Controllers
             }
             _db.SaveChanges();
             
-            return Json(new { success=true,message="Delete Successful"});
+            return Json(new { success=true,message="Operation Successful"});
         }
         #endregion
     }
